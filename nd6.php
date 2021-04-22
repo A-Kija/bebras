2151343460a2b602dcd88a55fdd16eee205df7bb
A-Kija <arvydas.kijakauskas@gmail.com>
2021-04-22 12:32:16 +0300

    6-7

--- nd6.php
+++ nd6.php
@@ -39,3 +39,35 @@ _d(howMuchIsTheFish(7), 'howMuchIsTheFish');
 _d(howMuchIsTheFish(12), 'howMuchIsTheFish');
 _d(howMuchIsTheFish(12), 'howMuchIsTheFish');
 _d(howMuchIsTheFish(3), 'howMuchIsTheFish');
+
+// Sugeneruokite atsitiktinio (nuo 10 iki 20) ilgio masyvą, kurio visi,
+//  išskyrus paskutinį, elementai yra atsitiktiniai skaičiai
+//   nuo 0 iki 10, o paskutinis masyvas, kuris generuojamas pagal
+//    tokią pat salygą kaip ir pirmasis masyvas. Viską
+//     pakartokite atsitiktinį nuo 10 iki 30  kiekį kartų. 
+
+
+function randomArr()
+{
+    $howLong = rand(10, 20);
+    $array = range(1, $howLong);
+    foreach ($array as &$value) {
+        $value = rand(0, 10);
+    }
+    return $array;
+}
+
+
+$howDeep = rand(10, 30) -1;
+$bigArray = randomArr();
+$bigArray[count($bigArray)-1] = 0;
+foreach(range(1, $howDeep) as $_) {
+    $nextArray = randomArr();
+    $nextArray[count($nextArray)-1] = $bigArray;
+    $bigArray = $nextArray;
+}
+
+
+
+_dc($bigArray);
+
