235d192e6dc54ae4561bfc80d55fe8ae36d05c37
A-Kija <arvydas.kijakauskas@gmail.com>
2021-05-25 12:31:44 +0300

    mada 2

--- mada/app/Http/Controllers/OutfitController.php
+++ mada/app/Http/Controllers/OutfitController.php
@@ -5,18 +5,69 @@ namespace App\Http\Controllers;
 use App\Models\Outfit;
 use App\Models\Master;
 use Illuminate\Http\Request;
+use Validator;
 
 class OutfitController extends Controller
 {
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $outfits = Outfit::all();
-        return view('outfit.index', ['outfits' => $outfits]);
+        
+        $masters = Master::all();
+
+        if ($request->master_id) {
+            $outfits = Outfit::where('master_id', $request->master_id)->get();
+            $master_id = $request->master_id;
+        }
+        else {
+            $outfits = Outfit::all();
+            $master_id = 0;
+        }
+
+        if ($request->sort) {
+            if ('asc' == $request->order) {
+                if ('size' == $request->sort) {
+                    $outfits = $outfits->sortBy('size');
+                    $order = 'asc';
+                    $sort = 'size';
+                }
+                elseif ('outfit' == $request->sort) {
+                    $outfits = $outfits->sortBy('type');
+                    $order = 'asc';
+                    $sort = 'outfit';
+                }
+            }
+            if ('desc' == $request->order) {
+                if ('size' == $request->sort) {
+                    $outfits = $outfits->sortByDesc('size');
+                    $order = 'desc';
+                    $sort = 'size';
+                }
+                elseif ('outfit' == $request->sort) {
+                    $outfits = $outfits->sortByDesc('type');
+                    $order = 'desc';
+                    $sort = 'outfit';
+                }
+            }
+        }
+
+        return view('outfit.index', [
+            'outfits' => $outfits,
+            'masters' => $masters,
+            'order' => $order ?? '',
+            'sort' => $sort ?? '',
+            'master_id' => $master_id,
+            
+            ]);
     }
 
     /**
@@ -38,6 +89,25 @@ class OutfitController extends Controller
      */
     public function store(Request $request)
     {
+        
+        $validator = Validator::make($request->all(),
+        [
+            'outfit_type' => ['required', 'min:3', 'max:50'],
+            'outfit_color' => ['required', 'min:2', 'max:20'],
+            'outfit_size' => ['required', 'integer', 'min:1', 'max:100'],
+            'outfit_about' => ['required'],
+            'master_id' => ['required', 'integer', 'min:1'],
+        ],
+        [
+            'master_id.min' => 'Please, select the Master'
+        ]
+        );
+
+        if ($validator->fails()) {
+            $request->flash();
+            return redirect()->back()->withErrors($validator);
+        }
+
         $outfit = new Outfit;
         $outfit->type = $request->outfit_type;
         $outfit->color = $request->outfit_color;
@@ -80,6 +150,24 @@ class OutfitController extends Controller
      */
     public function update(Request $request, Outfit $outfit)
     {
+        $validator = Validator::make($request->all(),
+        [
+            'outfit_type' => ['required', 'min:3', 'max:50'],
+            'outfit_color' => ['required', 'min:2', 'max:20'],
+            'outfit_size' => ['required', 'integer', 'min:1', 'max:100'],
+            'outfit_about' => ['required'],
+            'master_id' => ['required', 'integer', 'min:1'],
+        ],
+        [
+            'master_id.min' => 'Please, select the Master'
+        ]
+        );
+
+        if ($validator->fails()) {
+            $request->flash();
+            return redirect()->back()->withErrors($validator);
+        }
+        
         $outfit->type = $request->outfit_type;
         $outfit->color = $request->outfit_color;
         $outfit->size = $request->outfit_size;
--- mada/resources/views/outfit/create.blade.php
+++ mada/resources/views/outfit/create.blade.php
@@ -11,35 +11,36 @@
                     <form method="POST" action="{{route('outfit.store')}}">
                         <div class="form-group">
                             <label>Type</label>
-                            <input type="text" name="outfit_type" class="form-control">
+                            <input type="text" name="outfit_type" class="form-control" value="{{old('outfit_type')}}">
                             <small class="form-text text-muted">Outfit type (dress, shorts etc.)</small>
                         </div>
                         <div class="form-group">
                             <label>Color</label>
-                            <input type="text" name="outfit_color" class="form-control">
+                            <input type="text" name="outfit_color" class="form-control" value="{{old('outfit_color')}}">
                             <small class="form-text text-muted">Ottfit color</small>
                         </div>
                         <div class="form-group">
                             <label>Size</label>
-                            <input type="text" name="outfit_size" class="form-control">
+                            <input type="text" name="outfit_size" class="form-control" value="{{old('outfit_size')}}">
                             <small class="form-text text-muted">Otfit size (integer number 12, 48 etc.)</small>
                         </div>
                         <div class="form-group">
                             <label>About</label>
-                            <textarea name="master_name" id="summernote"></textarea>
+                            <textarea name="outfit_about" id="summernote">{{old('outfit_about')}}</textarea>
                             <small class="form-text text-muted">Client or Master comments about outfit</small>
                         </div>
 
                         <div class="form-group">
                             <label>Master</label>
                             <select class="form-select" name="master_id">
+                                <option value="0">Select Master</option>
                                 @foreach ($masters as $master)
-                                    <option value="{{$master->id}}">{{$master->name}} {{$master->surname}}</option>
+                                    <option value="{{$master->id}}" @if($master->id == old('master_id', 0)) selected @endif>{{$master->name}} {{$master->surname}}</option>
                                 @endforeach
                             </select>
                         </div>
                         @csrf
-                        <button type="submit"  class="btn btn-primary">MAKE</button>
+                        <button type="submit" class="btn btn-primary">MAKE</button>
                     </form>
                </div>
            </div>
